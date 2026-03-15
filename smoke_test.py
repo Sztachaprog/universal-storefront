ec2aeb12a42fcbef95f2551bba72aada06c0d900
Sztachansk <sztachaprograming@gmail.com>
2026-03-15 20:06:19 +0100

    manage files

--- smoke_test.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pytest
-import test_db
-from playwright.sync_api import sync_playwright
-
-def test_environment_ready():
-    print("\n[CONFIRMED] Ecosystem is alive!")
-    print(f"Requests version: {requests.__version__}")
---
+++ src/database.py
@@ -0,0 +1,7 @@
+connection_params = {
+    "host":"localhost",
+    "port": "5433",
+    "database":"storefront",
+    "user":"user",    
+    "password":"password"
+}
