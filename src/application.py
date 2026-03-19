31f47ba8c254f4a89f04ee2ea98ccba2aa4656a1
sztacha <sztachaprograming@gmail.com>
2026-03-19 18:06:31 +0100

    Update user table

--- src/application.py
+++ src/application.py
@@ -1,16 +1,15 @@
 
 from src.database import get_db_connection, close_db_connection
 
-def add_user(username, is_premium=False):
-    """Dodaje użytkownika do bazy i zwraca jego ID."""
+def add_user(username, password_hash, email, is_premium=False):
     conn = get_db_connection()
     if not conn:
         return None
         
     cursor = conn.cursor()
     try:
-        query = "INSERT INTO users (username, is_premium) VALUES (%s, %s) RETURNING id;"
-        cursor.execute(query, (username, is_premium))
+        query = "INSERT INTO users (username, password_hash, email, is_premium) VALUES (%s, %s, %s, %s) RETURNING id;"
+        cursor.execute(query, (username, password_hash, email, is_premium))
         user_id = cursor.fetchone()[0]
         conn.commit()
         return user_id
@@ -25,7 +24,7 @@ def get_user_by_id(user_id):
     """Pobiera dane użytkownika o konkretnym ID."""
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT id, username, is_premium FROM users WHERE id = %s;", (user_id,))
+    cursor.execute("SELECT id, username, email, is_premium FROM users WHERE id = %s;", (user_id,))
     user = cursor.fetchone()
     close_db_connection(conn, cursor)
     return user
