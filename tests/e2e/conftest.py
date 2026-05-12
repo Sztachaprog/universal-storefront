95f6a744e8860b3fcb7dc2ad486af25c8ca4b070
sztacha <sztachaprograming@gmail.com>
2026-05-12 22:00:37 +0200

    test: add TestUser dataclass and egistered_user fixture

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,5 +1,14 @@
 import pytest
+from src.application import register_user
 from src.database.database import get_db_connection, close_db_connection
+from dataclasses import dataclass
+
+@dataclass
+class TestUser:
+    username: str
+    password: str
+    email: str
+
 
 
 @pytest.fixture(scope="function", autouse=False)
@@ -17,6 +26,13 @@ def cursor(conn):
     conn.commit()
     close_db_connection(conn)
 
-    
-
-
+@pytest.fixture(scope="function", autouse=False)
+def registered_user(cursor, conn):
+    user = TestUser(
+        username="testusername", 
+        password="testpassword", 
+        email="testuser@mail.com"
+    )
+    register_user(user.username, user.password, user.email, is_premium = False, cursor=cursor)
+    conn.commit()
+    return user
