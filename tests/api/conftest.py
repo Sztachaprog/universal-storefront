042fe8ac18a5406bf66f9963fe165ff7ec3c0367
Sztachansk <sztachaprograming@gmail.com>
2026-06-06 19:49:29 +0200

    test: Creating space with conftest for API tests and test_api_get_user

---
+++ tests/api/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from src.database.database import get_db_connection, close_db_connection
+
+@pytest.fixture(scope="function", autouse=False)
+def conn():
+    conn = get_db_connection()
+    yield conn
+    close_db_connection(conn)
+
+@pytest.fixture(scope="function", autouse=True)
+def cursor(conn):
+    cursor = conn.cursor()
+
+    yield cursor
+
+    cursor.execute("TRUNCATE TABLE users, movies, user_access RESTART IDENTITY CASCADE;")
+    conn.commit()
+    close_db_connection(conn)
+
+
---
+++ tests/api/test_users_api.py
@@ -0,0 +1,13 @@
+from src.application import (
+    register_user
+)
+import requests
+
+def test_api_get_user(cursor, conn):
+
+   register_user("username1", "password1", "bartek@example.com", is_premium=False, cursor=cursor)
+   conn.commit()
+   response = requests.get("http://localhost:5000/api/users/1")
+
+
+   assert response.status_code == 200, "User should exist"
