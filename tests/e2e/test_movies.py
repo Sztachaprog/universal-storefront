c4e86e1003fdba48a124f430296133214b94a7a6
Sztachansk <sztachaprograming@gmail.com>
2026-08-14 14:41:40 +0200

    test(e2e): create access control test for premium user watch premium movie

--- tests/e2e/test_movies.py
+++ tests/e2e/test_movies.py
@@ -3,7 +3,10 @@ from playwright.sync_api import expect
 from tests.e2e.pages.movies_page import MoviesPage
 from tests.e2e.pages.login_page import LoginPage
 
+import allure 
 
+allure.feature("E2E movies")
+@allure.story("access control")
 def test_non_logged_user_cannot_enter_movie_watch(page, add_premium_and_non_premium_movies):
 
     non_premium_movie = add_premium_and_non_premium_movies[1]
@@ -11,7 +14,8 @@ def test_non_logged_user_cannot_enter_movie_watch(page, add_premium_and_non_prem
     page.goto(f"http://localhost:5000/movies/{non_premium_movie}/watch")
     expect(page).to_have_url(f"http://localhost:5000/login")
 
-
+allure.feature("E2E movies")
+@allure.story("access control")
 def test_non_premium_user_can_watch_non_premium_movie_via_direct_url(page, login_non_premium_user, add_premium_and_non_premium_movies):
 
     non_premium_movie = add_premium_and_non_premium_movies[1]
@@ -19,7 +23,8 @@ def test_non_premium_user_can_watch_non_premium_movie_via_direct_url(page, login
     page.goto(f"http://localhost:5000/movies/{non_premium_movie}/watch")
     expect(page).to_have_url(f"http://localhost:5000/movies/{non_premium_movie}/watch")
 
-
+allure.feature("E2E movies")
+@allure.story("access control")
 def test_non_premium_user_cannot_watch_premium_movie_via_direct_url(page, login_non_premium_user, add_premium_and_non_premium_movies):
 
     premium_movie = add_premium_and_non_premium_movies[0]
@@ -27,22 +32,11 @@ def test_non_premium_user_cannot_watch_premium_movie_via_direct_url(page, login_
     page.goto(f"http://localhost:5000/movies/{premium_movie}/watch")
     expect(page).to_have_url(f"http://localhost:5000/movies/{premium_movie}")
     
-# def test_premium_user_watches_non_premium_movie(page, login_premium_user, add_premium_and_non_premium_movies):
+allure.feature("E2E movies")
+@allure.story("access control")    
+def test_premium_user_cann_watch_premium_movie_via_direct_url(page, login_premium_user, add_premium_and_non_premium_movies):
 
-#     movies_page = MoviesPage(page)
+    premium_movie = add_premium_and_non_premium_movies[0]
 
-#     movies_page.navigate()
-#     movies_page.choose_non_premium_movie()
-#     movies_page.press_watch_button()
-
-#     assert page.locator("#player-screen").is_visible() == True, "Player screen should be visible"
-
-# def test_premium_user_watches_premium_movie(page, login_premium_user, add_premium_and_non_premium_movies):
-
-#     movies_page = MoviesPage(page)
-
-#     movies_page.navigate()
-#     movies_page.choose_premium_movie()
-#     movies_page.press_watch_button()
-
-#     assert page.locator("#player-screen").is_visible() == True, "Player screen should be visible"
+    page.goto(f"http://localhost:5000/movies/{premium_movie}/watch")
+    expect(page).to_have_url(f"http://localhost:5000/movies/{premium_movie}/watch")
